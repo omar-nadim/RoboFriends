2dda72061349074b00a496fce2eb57d4dd10ffac
Omar Nadim <omarnadim@Omars-MacBook-Air.local>
2020-08-05 12:21:18 +0200

    Finalizing and cleaning the App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js'
+import App from './Containers/App.js'
 import 'tachyons'; 
 import * as serviceWorker from './serviceWorker';
 
