3bd158fe291153c7281b96f7927eb253bef84381
Omar Nadim <omarnadim@Omars-MacBook-Air.local>
2020-08-06 09:46:40 +0200

    Updates

---
+++ precache-manifest.e29aef0024a14f41dc1e7ad038fa04df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6e01f8bb73d7666d07dc869b2720fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "31e5af0058c77dc847bb",
+    "url": "/static/css/main.46a6c107.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e5af0058c77dc847bb",
+    "url": "/static/js/main.ecbe952e.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
